cfbd97fce0cb2d2b97847fc824ddf49968a03097
Danielle Robinson <danni@Danielles-MacBook-Pro-2.local>
2013-10-08 10:28:09 -0700

    Initial Commit

---
+++ Temp Converter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Temp Converter
+//
+//  Created by Danielle Robinson on 8/10/13.
+//  Copyright (c) 2013 Danielle Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
